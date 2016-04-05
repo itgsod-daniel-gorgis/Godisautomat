ba6338c53af2f087004b27e1b2b3234332162d95
itgsod-daniel-gorgis <daniel.gorgis@elev.it-gymnasiet.se>
2016-04-05 11:04:01 +0200

    we are done

---
+++ godis.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+#randit 0,1 betyder att 50,50 chans
+
+
+#1 choklad 10 kr
+#2 fetis 8 kr
+
+
+print "choklad  10 kr"
+
+print "fetis 10 kr"
+
+print "lagg in pengar"
+
+print "tryck nr for onskad prudoct"
+
+nr= raw_input("vad vill du ha? OBS kolla att varan finns!:")
+
+money=raw_input("hur mycket pengar")
+# mata in pengar
+
+# kolla om pengar motsvarar varans kostnad
+
+if int(money) > 10:
+  print "Tack for dricksen!"
+
+if int(money) < 10:
+    print "Medges ej"
+
+if int(money) == 10:
+    print"Tack for ditt kop!"
+
+
+
+#tack ar defult och de kommer kunna kunna lagga in sa mycket pengar de vill.
